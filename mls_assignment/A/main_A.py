5467caca33cbaa14525d9aaa92c0db2238ad1dc7
NuhaAz <nuhaaisyah9@gmail.com>
2025-01-07 19:23:53 +0000

    Add: SVM w/ RBF Kernel

--- mls_assignment/A/main_A.py
+++ mls_assignment/A/main_A.py
@@ -5,10 +5,15 @@ import pandas as pd
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+import cv2
 
 # Import ML models
-from models import KNNClassifier, SVMClassifier 
+from models import *
 
+
+def convert_to_high_contrast(image):
+    _, bw_image = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
+    return bw_image
 
 # Load data
 file_loc = "../datasets/breastmnist.npz"
@@ -22,7 +27,7 @@ data = np.load(file_loc)
 # test_images   | (156x28x28)
 # test_labels   | (156x1)
 #----------------------------
-# Benign: 0, Malignant: 1
+# Malignant: 0, Benign: 1
 
 # Convert 2D array (image) into 1D array of pixel values for each data split
 x_train = np.array([np.hstack(x) for x in data["train_images"]])
@@ -31,6 +36,14 @@ x_val = np.array([np.hstack(x) for x in data["val_images"]])
 y_val = np.array(data["val_labels"]).ravel()
 x_test = np.array([np.hstack(x) for x in data["test_images"]])
 y_test = np.array(data["test_labels"]).ravel()
+
+# Attempt at preprocessing data (not used)
+# x_train_contrast = np.array([np.hstack(convert_to_high_contrast(x)) for x in data["train_images"]])
+# x_val_contrast = np.array([np.hstack(convert_to_high_contrast(x)) for x in data["val_images"]])
+# x_test_contrast = np.array([np.hstack(convert_to_high_contrast(x)) for x in data["test_images"]])
+# print(x_train[1])
+# print(x_train_contrast[1])
+# exit()
 
 model_sel = input("Model: ")
 accuracy_all = []
@@ -42,10 +55,8 @@ if model_sel == "1":
     # Training and validation of KNN model for different K Values
     K = [i for i in range(1,int(np.sqrt(len(x_train))))]
     scores = []
-    preds = []
     for k in K:
         y_pred = KNNClassifier(x_train, y_train, x_val, k)
-        preds.append(y_pred)
         scores.append(accuracy_score(y_val, y_pred))
 
     # Plotting validation set accuracy scores for different K Values
@@ -65,12 +76,12 @@ if model_sel == "1":
     y_pred = KNNClassifier(x_train, y_train, x_test, k)
     acccuracy = accuracy_score(y_test, y_pred)
     accuracy_all.append(acccuracy)
-    print("K Value: {}  |  Accuracy: {}".format(k, acccuracy))
+    print("Best K Value: {}  |  Accuracy: {}".format(k, acccuracy))
     
     # Create confusion matrix for test set
     con_mat = confusion_matrix(y_test, y_pred)
-    con_mat = pd.DataFrame(con_mat, columns=['Benign (True)', 'Malignant (True)'], 
-                                    index=['Benign (Predict)', 'Malignant (Predict)'])
+    con_mat = pd.DataFrame(con_mat, columns=['Malignant (True)', 'Benign (True)'], 
+                                    index=['Malignant (Predict)', 'Benign (Predict)'])
 
     fig, ax = plt.subplots()
     sns.heatmap(con_mat, annot=True, fmt='d', cmap='PuBu')
@@ -79,19 +90,87 @@ if model_sel == "1":
     plt.clf()
 
 elif model_sel == "2":
-    # SVM Classifier w/o Kernels
-    print("-------------SVM Classifier w/o Kernels-------------")
+    # Linear SVM Classifier
+    print("-------------Linear SVM Classifier-------------")
+    C_vals = [1e-1, 5e-1, 1, 5, 1e2]
     
-    # Training of SVM model
-    y_pred = SVMClassifier(x_train, y_train, x_test)
+    # Training and Validation of SVM model
+    # Validating for various C values
+    scores = []
+    for C in C_vals:
+        params = {'C': C}
+        y_pred = SVMClassifier(x_train, y_train, x_val, params)
+        acccuracy = accuracy_score(y_val, y_pred)
+        scores.append(acccuracy)
+        # print("Params: {}  |  Accuracy: {}".format(params, acccuracy))
+    
+    # Plotting validation set accuracy scores for different C Values
+    fig, ax = plt.subplots()
+    plt.plot(C_vals, scores)
+    plt.xlabel("C Value")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy vs C Value - BreastMNIST")
+    plt.grid()  
+    plt.show()
+    fig.savefig('accuracy.png')
+    plt.clf()
+
+    # Test set results based on C with highest accuracy (found during validation)
+    index = scores.index(max(scores))
+    param = {'C': C_vals[index]}
+    y_pred = SVMClassifier(x_train, y_train, x_test, param)
     acccuracy = accuracy_score(y_test, y_pred)
     accuracy_all.append(acccuracy)
-    print("Accuracy: {}".format(acccuracy))
+    print("Best Params: {}  |  Accuracy: {}".format(param, acccuracy))
     
     # Create confusion matrix
     con_mat = confusion_matrix(y_test, y_pred)
-    con_mat = pd.DataFrame(con_mat, columns=['Benign (True)', 'Malignant (True)'], 
-                                    index=['Benign (Predict)', 'Malignant (Predict)'])
+    con_mat = pd.DataFrame(con_mat, columns=['Malignant (True)', 'Benign (True)'], 
+                                    index=['Malignant (Predict)', 'Benign (Predict)'])
+
+    fig, ax = plt.subplots()
+    sns.heatmap(con_mat, annot=True, fmt='d', cmap='PuBu')
+    plt.show()
+    fig.savefig('confusion_matrix.png')
+    plt.clf()
+    
+elif model_sel == "3":    
+    print("-------------SVM Classifier w/ RBF Kernel-------------")
+    
+    # Training and Validation of SVM model
+    # Validating for various gamma values
+    C = 1
+    gamma_vals = [1e-1, 5e-1, 1, 5, 1e1]
+    scores = []
+    for gamma in gamma_vals:
+        params = {'C': C, 'gamma': gamma, 'kernel': 'rbf'}
+        y_pred = SVMClassifier(x_train, y_train, x_val, params)
+        acccuracy = accuracy_score(y_val, y_pred)
+        scores.append(acccuracy)
+            
+    # Plotting validation set accuracy scores for different gamma Values
+    fig, ax = plt.subplots()
+    plt.plot(gamma_vals, scores)
+    plt.xlabel("gamma")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy vs gamma - BreastMNIST")
+    plt.grid()  
+    plt.show()
+    fig.savefig('accuracy.png')
+    plt.clf()
+
+    # Test set results based on gamma with highest accuracy (found during validation)
+    index = scores.index(max(scores))
+    param = {'C': C, 'gamma': gamma_vals[index], 'kernel': 'rbf'}
+    y_pred = SVMClassifier(x_train, y_train, x_test, param)
+    acccuracy = accuracy_score(y_test, y_pred)
+    accuracy_all.append(acccuracy)
+    print("Best Params: {}  |  Accuracy: {}".format(param, acccuracy))
+    
+    # Create confusion matrix
+    con_mat = confusion_matrix(y_test, y_pred)
+    con_mat = pd.DataFrame(con_mat, columns=['Malignant (True)', 'Benign (True)'], 
+                                    index=['Malignant (Predict)', 'Benign (Predict)'])
 
     fig, ax = plt.subplots()
     sns.heatmap(con_mat, annot=True, fmt='d', cmap='PuBu')
--- mls_assignment/A/models.py
+++ mls_assignment/A/models.py
@@ -9,9 +9,14 @@ def KNNClassifier(x_train, y_train, x_test, k):
     y_pred = knn.predict(x_test)
     return y_pred
 
-# SVM Classifier function (w/o Kernels)
-def SVMClassifier(x_train,y_train, x_test):
-    model = svm.SVC()
+# SVM Classifier function (w/ Kernels)
+def SVMClassifier(x_train,y_train, x_test, params):
+    # Set default values for parameters
+    params.setdefault('C', 1)
+    params.setdefault('gamma', 1)
+    params.setdefault('kernel', 'linear')
+    
+    model = svm.SVC(C=params['C'], gamma=params['gamma'], kernel=params['kernel'])
     model.fit(x_train, y_train)
 
     y_pred = model.predict(x_test)
